bf6bb9882de3e15cb12e1cfad172236e2409c9cd
Niklas Sombert <niklas@ytvwld.de>
2023-04-04 23:31:20 +0200

    multiboot2: Support setting the image load address

--- multiboot2/src/image_load_addr.rs
+++ multiboot2/src/image_load_addr.rs
@@ -1,3 +1,8 @@
+use core::convert::TryInto;
+use core::mem::size_of;
+#[cfg(feature = "builder")]
+use crate::builder::traits::StructAsBytes;
+use crate::TagType;
 use crate::TagTypeId;
 
 /// If the image has relocatable header tag, this tag contains the image's
@@ -11,8 +16,25 @@ pub struct ImageLoadPhysAddr {
 }
 
 impl ImageLoadPhysAddr {
+    #[cfg(feature = "builder")]
+    pub fn new(load_base_addr: u32) -> Self {
+        Self {
+            typ: TagType::LoadBaseAddr,
+            size: size_of::<Self>().try_into().unwrap(),
+            load_base_addr,
+        }
+    }
+
     /// Returns the load base address.
     pub fn load_base_addr(&self) -> u32 {
         self.load_base_addr
     }
 }
+
+#[cfg(feature = "builder")]
+impl StructAsBytes for ImageLoadPhysAddr {
+    fn byte_size(&self) -> usize {
+        size_of::<Self>()
+    }
+}
+
